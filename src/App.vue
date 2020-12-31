c41d463bab4ddc34753f3f1e25ce36ed4013c434
seoly0 <seoly0.dev@gmail.com>
2021-01-01 07:21:32 +0900

    다른계정2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <div>오와아아아ㅏㄱ</div>
     <div>dmdmdmddm</div>
     <div>정신 나갈 것 같다</div>
+    <div>정신 나갈 것 같다</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
