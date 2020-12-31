972c334c51afba346529ec78fbcca72586bbc2c9
seoly0 <seoly0.dev@gmail.com>
2021-01-01 04:48:46 +0900

    last test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <div>jenkins test commit4</div>
+    <div>jenkins test commit5</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
